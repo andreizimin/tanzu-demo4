e6b0a58c139d3c5c98a3e2443502d25334704211
andreizimin <andrei.zimin@gmail.com>
2022-01-18 16:02:16 -0800

    message

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Tanzu Application Platform! Greetings from Spring Boot + Tanzu! Andrei Zimin";
+		return "Greetings from Spring Boot + Tanzu!";
 	}
 
 }
